b6f78567bccc88f1b367ab42ad04eed1ec994f0e
DipFellowAyo <103995759+DipFellowAyo@users.noreply.github.com>
2022-08-30 10:29:57 +0100

    new edits

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,26 +1,36 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/styles';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import loadable from '@loadable/component';
 
-import Loading from '../components/loading';
-import Myroutes from './routes';
+import {
+    LANDING_ROUTE,
+    PHOTOS_ROUTE
+} from "../utils/routes";
+
+// import Loading from '../components/loading';
+// import Myroutes from './routes';
 
 const theme = createTheme({
-    
+
 });
 
 
+
+const Home = loadable(() => import("../pages/home"));
+const Photos = loadable(() => import("../pages/photos"));
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline>
                 <BrowserRouter>
-                    <Suspense fallback={<Loading />}>
-                        <Myroutes />
-                    </Suspense>
+                    <Routes>
+                        <Route exact path={LANDING_ROUTE} element={<Home />} />
+                        <Route exact path={PHOTOS_ROUTE} element={<Photos />} />
+                    </Routes>
                 </BrowserRouter>
             </CssBaseline> 
         </ThemeProvider>
